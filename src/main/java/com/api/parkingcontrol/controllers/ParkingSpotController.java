411fe2c228cbb7be2efdb9a30b74abb72fff96b3
Luis Scudeler <luisscudeler@yahoo.com.br>
2023-03-16 15:21:54 -0300

    Pageable

--- src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
+++ src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -6,6 +6,11 @@ import com.api.parkingcontrol.services.ParkingSpotService;
 import org.apache.coyote.Response;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -44,8 +49,8 @@ public class ParkingSpotController {
     }
 
     @GetMapping
-    public ResponseEntity<List<ParkingSpotModel>> getAllPArkingSpots(){
-        return ResponseEntity.status((HttpStatus.OK)).body(parkingSpotService.findAll());
+    public ResponseEntity<Page<ParkingSpotModel>> getAllPArkingSpots(@PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable){
+        return ResponseEntity.status((HttpStatus.OK)).body(parkingSpotService.findAll(pageable));
     }
 
     @GetMapping("/{id}")
