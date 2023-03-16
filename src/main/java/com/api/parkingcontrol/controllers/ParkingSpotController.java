1e9c74571cbfe0a690589aa42ae0df528f9cbe19
Luis Scudeler <luisscudeler@yahoo.com.br>
2023-03-16 11:31:42 -0300

    Implementing Save parkingSpot

--- src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
+++ src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -1,10 +1,17 @@
 package com.api.parkingcontrol.controllers;
 
+import com.api.parkingcontrol.dto.ParkingSpotDTO;
+import com.api.parkingcontrol.model.ParkingSpotModel;
 import com.api.parkingcontrol.services.ParkingSpotService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600) //permite que seja acessado de qualquer fonte
@@ -13,6 +20,14 @@ public class ParkingSpotController {
 
     @Autowired
     private ParkingSpotService parkingSpotService;
+
+    @PostMapping
+    public ResponseEntity<Object> saveParkingSpot(@RequestBody @Valid ParkingSpotDTO parkingSpotDTO){ //esse @Valid fará com que as validações que fizemos na classe parkingModelDTO sejam efetuadas
+        var parkingSpotModel = new ParkingSpotModel(); //esse var é uma atualização do JDK. Dessa forma é o mesmo que fazer: ParkingSpotModel parkingSpotModel = new ParkingSpotModel();
+        BeanUtils.copyProperties(parkingSpotDTO, parkingSpotModel);
+        parkingSpotModel.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC")));
+        return ResponseEntity.status(HttpStatus.CREATED).body(parkingSpotService.save(parkingSpotModel));
+    }
 
 
 
--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -1,14 +1,22 @@
 package com.api.parkingcontrol.services;
 
+import com.api.parkingcontrol.model.ParkingSpotModel;
 import com.api.parkingcontrol.repositories.ParkingSpotRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 public class ParkingSpotService {
 
     @Autowired
     private ParkingSpotRepository parkingSpotRepository;
+
+    @Transactional //usado em metodos construtivos ou destrutivos pois caso dê errado ele garante o rollback
+    public ParkingSpotModel save(ParkingSpotModel parkingSpotModel){
+        return parkingSpotRepository.save(parkingSpotModel);
+    }
 
 
 }
